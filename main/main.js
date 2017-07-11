ae7ff77d67c3e850a991494073d6fd44dd6d37b9
WuYiChenhml <1055200502@qq.com>
2017-07-11 16:35:09 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,7 +1,6 @@
-module.exports = function main() {
-    
-    var a = prompt('输入被除数a', ' ');
-	var b =prompt('输入除数b', ' ');
-	var c = a%b;
-	return c;
-};
+module.exports = function main(a, b) {
+     var result = a % b;
+    return result;            //  返回值
+}
+var ans = main(9, 3);    // 调用函数，将返回值赋值给变量 ans
+console.log(ans);     
