503f322d7213db180e8fb863e33e7d5e5deee548
WuYiChenhml <1055200502@qq.com>
2017-07-11 17:01:38 +0800

    Update main-spec.js

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -9,11 +9,3 @@ describe('validate the file exist', () => {
         })
     });
 });
-var num1 = 9;
-var num2 = 2;
-function main(a, b) {
-     var result = a % b;
-    return result;            //  返回值
-};
-var ans = main(num1, num2);    // 调用函数，将返回值赋值给变量 ans
-console.log(ans);     
