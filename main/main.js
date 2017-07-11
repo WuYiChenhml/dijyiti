0b403765a845a115783ba6acd065686c97f189ea
WuYiChenhml <1055200502@qq.com>
2017-07-11 17:00:09 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -3,6 +3,6 @@ var num2 = 2;
 function main(a, b) {
      var result = a % b;
     return result;            //  返回值
-}
+};
 var ans = main(num1, num2);    // 调用函数，将返回值赋值给变量 ans
 console.log(ans);     
