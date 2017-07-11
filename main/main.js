9366f974ca8cf0c4a932de8cdb4ec0ff6e34576b
WuYiChenhml <1055200502@qq.com>
2017-07-11 14:49:58 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 module.exports = function main() {
     console.log("Debug Info");
     return 'Hello World!';
+    var a = '9';
+	a = Number(a);
+	var b = '4';
+	b = Number(b);
+	c = Math.floor(a/b);
+	d = a-b*c;
+    return 'd';
 };
